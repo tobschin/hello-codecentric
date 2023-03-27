ea314e15cef77a446001ea20583a3d44ffdf4ac4
tobiasschinagl <tobias.schinagl@codecentric.de>
2023-03-27 10:55:57 +0200

    logo as environment-var

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  console.log(process.env.REACT_APP_IMAGE_SRC)
   return (
     <div className="App">
       <header className="App-header">
-        <img src='https://www.nik-nbg.de/fileadmin/_processed_/6/1/csm_16-9_codecentri-Logo_8683eda41e.png' className="App-logo" alt="logo" />
+        <img src={process.env.REACT_APP_IMAGE_SRC} className="App-logo" alt="logo" />
         <p>
           Congrats you just deployed me.
         </p>
